c2b711c0a11f3980a8bf4c7a2acf85d80732620a
Rming <rmingwang@gmail.com>
2021-12-31 18:56:46 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-python simple_extractor.py --dataset atr --model-restore ./pretrain_model/atr.pth  --input-dir ./images --output-dir ./output/atr
-
-python simple_extractor.py --dataset lip --model-restore ./pretrain_model/lip.pth  --input-dir ./images --output-dir ./output/lip
-
-python simple_extractor.py --dataset pascal --model-restore ./pretrain_model/pascal.pth  --input-dir ./images --output-dir ./output/pascal
+python simple_extractor.py --dataset atr --model-restore ./pretrain_model/atr_abn.pth  --input-dir ./images --output-dir ./output
